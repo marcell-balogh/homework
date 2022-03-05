0999f51420f1deb5ef67d467a14157d35863a7e5
marcell-balogh <bamarci10@gmail.com>
2022-03-05 22:46:27 +0100

    refactoring

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Movie } from 'src/app/models/movie';
+import { SearchService } from 'src/app/services/search.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  query: string = '';
+  movies: Movie[] = [];
+  loading: boolean = false;
+  error: any;
 
-  constructor() { }
+  constructor(
+    private searchService: SearchService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.query = params.query;
+      console.log(this.query);
+      this.searchMovies();
+    });
   }
 
+  searchMovies() {
+    this.searchService
+      .searchMoviesQuery(this.query)
+      .valueChanges.subscribe(({ data, loading, error }) => {
+        this.loading = loading;
+        if (data) {
+          this.movies = data.searchMovies;
+        }
+        this.error = error;
+      });
+  }
 }
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,33 +1,25 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Apollo, gql, QueryRef } from 'apollo-angular';
 import { Observable } from 'rxjs';
-import { Movie } from '../models/movie';
-import { SearchService } from '../services/search.service';
+import { Movie } from '../../models/movie';
+import { SearchService } from '../../services/search.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent implements OnInit, OnDestroy {
+export class SearchComponent implements OnInit {
   public movies: Movie[] = [];
   public loading: boolean = false;
   public error: any;
   query: String = '';
 
-  constructor(private searchService: SearchService) {}
-  ngOnDestroy(): void {}
-  ngOnInit(): void {}
-
-  searchMovies() {
-    this.searchService
-      .searchMoviesQuery(this.query)
-      .valueChanges.subscribe(({ data, loading, error }) => {
-        this.loading = loading;
-        if (data) {
-          this.movies = data.searchMovies;
-        }
-        this.error = error;
-      });
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.query = params.query;
+    });
   }
 }
